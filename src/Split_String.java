2581bd6b4cfab763d8ca28b663e71943f19ba736
I432 <s94075@gmail.com>
2017-10-16 15:03:51 +0800

    20171016

---
+++ src/Split_String.java
@@ -0,0 +1,13 @@
+public class Split_String {
+
+    public static void main(String[] args){
+        //顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        String ch = sc.next();
+
+        int v1 = Integer.valueOf(ch, 16);
+
+        System.out.println((char) v1);
+    }
+}
